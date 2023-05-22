59b32190faeb1b1f651cfd9ded1539eb4a876f46
Felipe Guilherme <felipealguilherme@gmail.com>
2023-05-21 22:46:20 -0300

    Lista de exercícios em Python

---
+++ Exercício Lista 1 e 2 em Python/Exercícios Lista 1/Exercício 1.py
@@ -0,0 +1,11 @@
+# 1. Fazer um algoritmo para somar 2 números inteiros lidos pelo teclado.
+
+
+valor1 = input ('Digite o primeiro número: ')
+valor2 = input ('Digite o segundo número: ')
+
+
+resultado = int(valor1) + int(valor2)
+print('Sua soma ficou: ' + str(resultado))
+
+
